4f9ea991e1a1e58fd68ff1805f8e95df0fae27ef
Scott Halgrim <shalgrim@gmail.com>
2020-04-08 19:43:55 -0700

    thru ch 8

--- exercises/ch-8-ex-2/protectedResource.js
+++ exercises/ch-8-ex-2/protectedResource.js
@@ -10,6 +10,7 @@ var request = require("sync-request");
 var __ = require('underscore');
 var base64url = require('base64url');
 var jose = require('jsrsasign');
+var cors = require('cors')
 
 var app = express();
 
@@ -85,7 +86,9 @@ var requireAccessToken = function(req, res, next) {
 	}
 }; 
 
-app.get("/helloWorld", getAccessToken, function(req, res){
+app.options('/helloWorld', cors());
+app.get("/helloWorld", cors(), getAccessToken, function(req, res){
+// app.get("/helloWorld", getAccessToken, function(req, res){
 	if (req.access_token) {
 		
 		res.setHeader('X-Content-Type-Options','nosniff');
--- exercises/ch-8-ex-3/protectedResource.js
+++ exercises/ch-8-ex-3/protectedResource.js
@@ -89,9 +89,9 @@ app.options('/helloWorld', cors());
 
 app.get("/helloWorld", cors(), getAccessToken, function(req, res){
 	if (req.access_token) {
-		
 		res.setHeader('X-Content-Type-Options','nosniff');
 		res.setHeader('X-XSS-Protection', '1; mode=block');
+		res.setHeader('Strict-Transport-Security', 'max-age=31536000');
 
 		var resource = {
 			"greeting" : ""
