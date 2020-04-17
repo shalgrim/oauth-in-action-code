ef66f2e24f4e79fc802f9cb7f9923a38955d6322
Scott Halgrim <scott.halgrim@zapier.com>
2020-04-17 12:36:30 -0700

    thru ch 15

--- exercises/ch-15-ex-1/client.js
+++ exercises/ch-15-ex-1/client.js
@@ -1,40 +1,40 @@
 var express = require("express");
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 var request = require("sync-request");
 var url = require("url");
 var qs = require("qs");
-var querystring = require('querystring');
-var cons = require('consolidate');
+var querystring = require("querystring");
+var cons = require("consolidate");
 var randomstring = require("randomstring");
-var __ = require('underscore');
-__.string = require('underscore.string');
-var jose = require('jsrsasign');
+var __ = require("underscore");
+__.string = require("underscore.string");
+var jose = require("jsrsasign");
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.engine('html', cons.underscore);
-app.set('view engine', 'html');
-app.set('views', 'files/client');
+app.engine("html", cons.underscore);
+app.set("view engine", "html");
+app.set("views", "files/client");
 
 // authorization server information
 var authServer = {
-	authorizationEndpoint: 'http://localhost:9001/authorize',
-	tokenEndpoint: 'http://localhost:9001/token'
+  authorizationEndpoint: "http://localhost:9001/authorize",
+  tokenEndpoint: "http://localhost:9001/token",
 };
 
 // client information
 
 var client = {
-	"client_id": "oauth-client-1",
-	"client_secret": "oauth-client-secret-1",
-	"redirect_uris": ["http://localhost:9000/callback"],
-	"scope": "foo"
+  client_id: "oauth-client-1",
+  client_secret: "oauth-client-secret-1",
+  redirect_uris: ["http://localhost:9000/callback"],
+  scope: "foo",
 };
 
-var protectedResource = 'http://localhost:9002/resource';
+var protectedResource = "http://localhost:9002/resource";
 
 var state = null;
 
@@ -44,145 +44,175 @@ var scope = null;
 var key = null;
 var alg = null;
 
-app.get('/', function (req, res) {
-	res.render('index', {access_token: access_token, refresh_token: refresh_token, scope: scope, key: key});
+app.get("/", function (req, res) {
+  res.render("index", {
+    access_token: access_token,
+    refresh_token: refresh_token,
+    scope: scope,
+    key: key,
+  });
 });
 
-app.get('/authorize', function(req, res){
+app.get("/authorize", function (req, res) {
+  access_token = null;
+  refresh_token = null;
+  scope = null;
+  state = randomstring.generate();
 
-	access_token = null;
-	refresh_token = null;
-	scope = null;
-	state = randomstring.generate();
-	
-	var authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
-		response_type: 'code',
-		scope: client.scope,
-		client_id: client.client_id,
-		redirect_uri: client.redirect_uris[0],
-		state: state
-	});
-	
-	console.log("redirect", url.format(authorizeUrl));
-	res.redirect(url.format(authorizeUrl));
+  var authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
+    response_type: "code",
+    scope: client.scope,
+    client_id: client.client_id,
+    redirect_uri: client.redirect_uris[0],
+    state: state,
+  });
+
+  console.log("redirect", url.format(authorizeUrl));
+  res.redirect(url.format(authorizeUrl));
 });
 
-app.get("/callback", function(req, res){
-	
-	if (req.query.error) {
-		// it's an error response, act accordingly
-		res.render('error', {error: req.query.error});
-		return;
-	}
-	
-	var resState = req.query.state;
-	if (resState == state) {
-		console.log('State value matches: expected %s got %s', state, resState);
-	} else {
-		console.log('State DOES NOT MATCH: expected %s got %s', state, resState);
-		res.render('error', {error: 'State value did not match'});
-		return;
-	}
+app.get("/callback", function (req, res) {
+  if (req.query.error) {
+    // it's an error response, act accordingly
+    res.render("error", { error: req.query.error });
+    return;
+  }
 
-	var code = req.query.code;
+  var resState = req.query.state;
+  if (resState == state) {
+    console.log("State value matches: expected %s got %s", state, resState);
+  } else {
+    console.log("State DOES NOT MATCH: expected %s got %s", state, resState);
+    res.render("error", { error: "State value did not match" });
+    return;
+  }
 
-	var form_data = qs.stringify({
-		grant_type: 'authorization_code',
-		code: code,
-		redirect_uri: client.redirect_uri
-	});
-	var headers = {
-		'Content-Type': 'application/x-www-form-urlencoded',
-		'Authorization': 'Basic ' + encodeClientCredentials(client.client_id, client.client_secret)
-	};
+  var code = req.query.code;
 
-	var tokRes = request('POST', authServer.tokenEndpoint, {	
-		body: form_data,
-		headers: headers
-	});
+  var form_data = qs.stringify({
+    grant_type: "authorization_code",
+    code: code,
+    redirect_uri: client.redirect_uri,
+  });
+  var headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Authorization:
+      "Basic " +
+      encodeClientCredentials(client.client_id, client.client_secret),
+  };
 
-	console.log('Requesting access token for code %s',code);
-	
-	if (tokRes.statusCode >= 200 && tokRes.statusCode < 300) {
-		var body = JSON.parse(tokRes.getBody());
-	
-		access_token = body.access_token;
-		console.log('Got access token: %s', access_token);
-		if (body.refresh_token) {
-			refresh_token = body.refresh_token;
-			console.log('Got refresh token: %s', refresh_token);
-		}
-		
-		scope = body.scope;
-		console.log('Got scope: %s', scope);
+  var tokRes = request("POST", authServer.tokenEndpoint, {
+    body: form_data,
+    headers: headers,
+  });
 
-		/*
-		 * Save the access token key
-		 */
+  console.log("Requesting access token for code %s", code);
 
-		res.render('index', {access_token: access_token, refresh_token: refresh_token, scope: scope, key: key});
-	} else {
-		res.render('error', {error: 'Unable to fetch access token, server response: ' + tokRes.statusCode})
-	}
+  if (tokRes.statusCode >= 200 && tokRes.statusCode < 300) {
+    var body = JSON.parse(tokRes.getBody());
+
+    access_token = body.access_token;
+    console.log("Got access token: %s", access_token);
+    if (body.refresh_token) {
+      refresh_token = body.refresh_token;
+      console.log("Got refresh token: %s", refresh_token);
+    }
+
+    scope = body.scope;
+    console.log("Got scope: %s", scope);
+
+    /*
+     * Save the access token key
+     */
+    key = body.access_token_key;
+    alg = body.alg;
+
+    res.render("index", {
+      access_token: access_token,
+      refresh_token: refresh_token,
+      scope: scope,
+      key: key,
+    });
+  } else {
+    res.render("error", {
+      error:
+        "Unable to fetch access token, server response: " + tokRes.statusCode,
+    });
+  }
 });
 
-app.get('/fetch_resource', function(req, res) {
+app.get("/fetch_resource", function (req, res) {
+  if (!access_token) {
+    res.render("error", { error: "Missing access token." });
+    return;
+  }
 
-	if (!access_token) {
-		res.render('error', {error: 'Missing access token.'});
-		return;
-	}
-	
-	/*
-	 * Create a signed HTTP object and add it to the headers of the request
-	 */
+  /*
+   * Create a signed HTTP object and add it to the headers of the request
+   */
+  var header = { typ: "PoP", alg: alg, kid: key.kid };
 
-	var headers = {
-		'Content-Type': 'application/x-www-form-urlencoded'
-	};
-	
-	var resource = request('POST', protectedResource,
-		{headers: headers}
-	);
-	
-	if (resource.statusCode >= 200 && resource.statusCode < 300) {
-		var body = JSON.parse(resource.getBody());
-		res.render('data', {resource: body});
-		return;
-	} else {
-		access_token = null;
-		res.render('error', {error: 'Server returned response code: ' + resource.statusCode});
-		return;
-	}
-	
-	
+  var payload = {};
+  payload.at = access_token;
+  payload.ts = Matt.floor(Date.now() / 1000);
+  payload.m = "POST";
+  payload.u = "localhost:9002";
+  payload.p = "/resource";
+
+  var privateKey = jose.KEYUTIL.getKey(key);
+  var signed = jose.jws.JWS.sign(
+    alg,
+    JSON.stringify(header),
+    JSON.stringify(payload),
+    privateKey
+  );
+
+  var headers = {
+    Authorization: "PoP " + signed,
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+
+  var resource = request("POST", protectedResource, { headers: headers });
+
+  if (resource.statusCode >= 200 && resource.statusCode < 300) {
+    var body = JSON.parse(resource.getBody());
+    res.render("data", { resource: body });
+    return;
+  } else {
+    access_token = null;
+    res.render("error", {
+      error: "Server returned response code: " + resource.statusCode,
+    });
+    return;
+  }
 });
 
-var buildUrl = function(base, options, hash) {
-	var newUrl = url.parse(base, true);
-	delete newUrl.search;
-	if (!newUrl.query) {
-		newUrl.query = {};
-	}
-	__.each(options, function(value, key, list) {
-		newUrl.query[key] = value;
-	});
-	if (hash) {
-		newUrl.hash = hash;
-	}
-	
-	return url.format(newUrl);
+var buildUrl = function (base, options, hash) {
+  var newUrl = url.parse(base, true);
+  delete newUrl.search;
+  if (!newUrl.query) {
+    newUrl.query = {};
+  }
+  __.each(options, function (value, key, list) {
+    newUrl.query[key] = value;
+  });
+  if (hash) {
+    newUrl.hash = hash;
+  }
+
+  return url.format(newUrl);
 };
 
-var encodeClientCredentials = function(clientId, clientSecret) {
-	return new Buffer(querystring.escape(clientId) + ':' + querystring.escape(clientSecret)).toString('base64');
+var encodeClientCredentials = function (clientId, clientSecret) {
+  return new Buffer(
+    querystring.escape(clientId) + ":" + querystring.escape(clientSecret)
+  ).toString("base64");
 };
 
-app.use('/', express.static('files/client'));
+app.use("/", express.static("files/client"));
 
-var server = app.listen(9000, 'localhost', function () {
+var server = app.listen(9000, "localhost", function () {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('OAuth Client is listening at http://%s:%s', host, port);
+  console.log("OAuth Client is listening at http://%s:%s", host, port);
 });
- 
--- exercises/ch-15-ex-1/protectedResource.js
+++ exercises/ch-15-ex-1/protectedResource.js
@@ -1,77 +1,120 @@
 var express = require("express");
-var bodyParser = require('body-parser');
-var cons = require('consolidate');
+var bodyParser = require("body-parser");
+var cons = require("consolidate");
 var qs = require("qs");
-var querystring = require('querystring');
+var querystring = require("querystring");
 var request = require("sync-request");
-var __ = require('underscore');
-var base64url = require('base64url');
-var jose = require('jsrsasign');
-var cors = require('cors');
+var __ = require("underscore");
+var base64url = require("base64url");
+var jose = require("jsrsasign");
+var cors = require("cors");
 
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true })); // support form-encoded bodies (for bearer tokens)
 
-app.engine('html', cons.underscore);
-app.set('view engine', 'html');
-app.set('views', 'files/protectedResource');
-app.set('json spaces', 4);
+app.engine("html", cons.underscore);
+app.set("view engine", "html");
+app.set("views", "files/protectedResource");
+app.set("json spaces", 4);
 
-app.use('/', express.static('files/protectedResource'));
+app.use("/", express.static("files/protectedResource"));
 app.use(cors());
 
 var resource = {
-	"name": "Protected Resource",
-	"description": "This data has been protected by OAuth 2.0"
+  name: "Protected Resource",
+  description: "This data has been protected by OAuth 2.0",
 };
 
 var protectedResource = {
-		"resource_id": "protected-resource-1",
-		"resource_secret": "protected-resource-secret-1"
+  resource_id: "protected-resource-1",
+  resource_secret: "protected-resource-secret-1",
 };
 
 var authServer = {
-	introspectionEndpoint: 'http://localhost:9001/introspect'
+  introspectionEndpoint: "http://localhost:9001/introspect",
 };
 
+var getAccessToken = function (req, res, next) {
+  /*
+   * Implement PoP signature validation and token lookup using introspection
+   */
+  var auth = req.headers["authorization"];
+  var inToken = null;
+  if (auth && auth.toLowerCase().indexOf("pop") == 0) {
+    inToken = auth.slice("pop ".length);
+  } else if (req.body && req.body.pop_access_token) {
+    inToken = req.body.pop_access_token;
+  } else if (req.query && req.query.pop_access_token) {
+    inToken = req.query.pop_access_token;
+  }
 
-var getAccessToken = function(req, res, next) {
+  var tokenParts = inToken.split(".");
+  var header = JSON.parse(base64url.decode(tokenParts[0]));
+  var payload = JSON.parse(base64ur.decode(tokenParts[1]));
+  var at = payload.at;
 
-	/*
-	 * Implement PoP signature validation and token lookup using introspection
-	 */
+  var form_data = qs.stringify({ token: at });
+  var headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Authorization:
+      "Basic " +
+      encodeClientCredentials(
+        protectedResource.resource_id,
+        protectedResource.resource_secret
+      ),
+  };
+  var tokRes = request("POST", authServer.introspectionEndpoint, {
+    body: form_data,
+    headers: headers,
+  });
 
+  if (tokRes.statusCode >= 200 && tokRes.statusCode < 300) {
+	  var body = JSON.parse(tokRes.getBody());
+
+	  var active = body.active;
+	  if (active) {
+		  var pubKey = jose.KEYUTIL.getKey(body.access_token_key);
+		  if (jose.jws.JWS.verify(inToken, pubKey, [header.alg])) {
+			  if (!payload.m || payload.m == req.method) {
+				  if (!payload.u || payload.u == 'localhost:9002') {
+					  if (!payload.p || paylaad.p == req.path) {
+						  req.access_token = {access_token: at, scope: body.scope};
+					  }
+				  }
+			  }
+		  }
+	  }
+  }
 };
 
-var requireAccessToken = function(req, res, next) {
-	if (req.access_token) {
-		next();
-	} else {
-		res.status(401).end();
-	}
+var requireAccessToken = function (req, res, next) {
+  if (req.access_token) {
+    next();
+  } else {
+    res.status(401).end();
+  }
 };
 
-app.options('/resource', cors());
+app.options("/resource", cors());
 
-app.post("/resource", cors(), getAccessToken, function(req, res){
-
-	if (req.access_token) {
-		res.json(resource);
-	} else {
-		res.status(401).end();
-	}
-	
+app.post("/resource", cors(), getAccessToken, function (req, res) {
+  if (req.access_token) {
+    res.json(resource);
+  } else {
+    res.status(401).end();
+  }
 });
 
-var encodeClientCredentials = function(clientId, clientSecret) {
-	return new Buffer(querystring.escape(clientId) + ':' + querystring.escape(clientSecret)).toString('base64');
+var encodeClientCredentials = function (clientId, clientSecret) {
+  return new Buffer(
+    querystring.escape(clientId) + ":" + querystring.escape(clientSecret)
+  ).toString("base64");
 };
 
-var server = app.listen(9002, 'localhost', function () {
+var server = app.listen(9002, "localhost", function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('OAuth Resource Server is listening at http://%s:%s', host, port);
+  console.log("OAuth Resource Server is listening at http://%s:%s", host, port);
 });
- 
